e6f266bd0f0ed5fefb671f025b2e309c4e00a35f
Sonal Keraliya <sonal@mygreenbutler.com>
2022-09-04 09:03:28 +1000

    add react router

--- superhero-frontend/src/App.js
+++ superhero-frontend/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +19,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Router>
   );
 }
 
