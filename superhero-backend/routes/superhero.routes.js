08571692fbc439f2891c0a8f41496741f8bd36c6
Sonal Keraliya <sonal@mygreenbutler.com>
2022-09-04 11:01:14 +1000

    create route path for api

--- superhero-backend/routes/superhero.routes.js
+++ superhero-backend/routes/superhero.routes.js
@@ -1,5 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const superheros = require('../controllers/superhero.contoller.js')
 
+
+ // Create a new SuperHero
+ router.post("/", superheros.createHero);
+
+ // Update a Super Hero with id
+ router.put("/:id", superheros.updateHero);
+
+  //Retrieve all Superheros
+ router.get("/", superheros.findAllHero);
+
+
+ // Retrieve a single Super Hero with id
+ router.get("/:id", superheros.findOneHero);
+ 
+ // Delete a Super Hero with id
+ router.delete("/:id", superheros.deleteHero); 
 
 module.exports = router
